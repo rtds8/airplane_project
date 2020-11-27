6d912e59c98afc98300aed4f3c150d042875e929
saradesu7 <64678679+saradesu7@users.noreply.github.com>
2020-11-27 18:13:17 +0530

    CineMachine + CamMove on Play

---
+++ Assets/Airplane/Scripts/Movecam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Movecam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CinemachineVirtualCamera currentCamera;
+    public CinemachineBrain cBrain;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dolly = currentCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+        dolly.m_PathPosition += 0.005f;
+    }
+}
